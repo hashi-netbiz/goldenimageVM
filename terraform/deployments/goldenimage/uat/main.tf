42d2ca3476fb7ab7fdab35108ae29a5f451792ef
hashi-netbiz <116410436+hashi-netbiz@users.noreply.github.com>
2023-06-27 04:19:17 -0400

    Update main.tf

--- terraform/deployments/goldenimage/uat/main.tf
+++ terraform/deployments/goldenimage/uat/main.tf
@@ -35,10 +35,10 @@ terraform {
 provider "azurerm" {
   features {}
   
-  #subscription_id = var.ARM_SUBSCRIPTION_ID
-  #client_id       = var.ARM_CLIENT_ID
-  #client_secret   = var.ARM_CLIENT_SECRET
-  #tenant_id       = var.ARM_TENANT_ID
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
 }
 
 # export environmental vars as TF_VAR_VAULT_ADDR and TF_VARS_VAULT_TOKEN
